ef6a2dd7334822d26606f5bd0781f251173c68e8
David Buchmann <david.buchmann@liip.ch>
2014-05-09 16:12:23 +0200

    adding conference bundle and fixtures for some pages

---
+++ src/Dbu/ConferenceBundle/DataFixtures/PHPCR/LoadOverviewData.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Sandbox\MainBundle\DataFixtures\PHPCR;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use PHPCR\Util\PathHelper;
+use Symfony\Cmf\Bundle\SimpleCmsBundle\Doctrine\Phpcr\Page;
+use Symfony\Component\DependencyInjection\ContainerAware;
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\ODM\PHPCR\DocumentManager;
+use PHPCR\Util\NodeHelper;
+
+class LoadOverviewData extends ContainerAware implements FixtureInterface, OrderedFixtureInterface
+{
+    public function getOrder()
+    {
+        return 10;
+    }
+
+    /**
+     * @param DocumentManager $manager
+     */
+    public function load(ObjectManager $manager)
+    {
+        $path = $this->container->getParameter('dbu_conference.home_path');
+
+        $page = $manager->find(null, $path);
+        $page->setTitle('Our lovely conference');
+        $page->setBody('Welcome to the conference website.');
+
+        $schedule = new Page();
+        $schedule->setPosition($page, 'schedule');
+        $schedule->setLabel('Schedule');
+        $schedule->setTitle('Conference Schedule');
+        $schedule->setBody('');
+        $manager->persist($schedule);
+
+        $schedule = new Page();
+        $schedule->setPosition($page, 'speakers');
+        $schedule->setLabel('Speakers');
+        $schedule->setTitle('Your friendly speakers');
+        $schedule->setBody('');
+        $manager->persist($schedule);
+
+        $manager->flush();
+    }
+}
